511b3b2edeb7b3c5970fc4dd4cd3a97335418327
Amol Barge <bargeamol2000@gmail.com>
2020-08-19 19:01:12 +0530

    m

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Server running at ${port}/`);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
